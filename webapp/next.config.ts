d314bf8bd54e05e2611eb3564672e9ba3223d4c0
Ganesh K <ganeshkhovalan2203@gmail.com>
2026-03-09 10:48:05 +0530

    commit

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://ec2-3-233-219-49.compute-1.amazonaws.com:8000/api/v1/:path*", // AWS EC2 Backend
+        destination: "https://api.adhi.rootedai.co.in/api/v1/:path*", // AWS ALB (HTTPS)
       },
     ];
   },
